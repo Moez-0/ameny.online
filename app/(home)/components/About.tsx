a97a0dc1698e3a38f1826ed112cbc5cd866597d6
Moez-0 <moezlegend@gmail.com>
2024-11-07 11:47:56 +0100

    changed herdo

---
+++ app/(home)/components/About.tsx
@@ -0,0 +1,46 @@
+import IconCloud from '@/components/ui/icon-cloud'
+import NumberTicker from '@/components/ui/number-ticker'
+import React from 'react'
+ 
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+const About = () => {
+  return (
+    <div className='md:p-24 p-14'>
+
+    
+    </div>
+  )
+}
+
+export default About
