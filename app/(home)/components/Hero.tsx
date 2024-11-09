8e16f9e9d73b4a366ece19e93db29aa451239c3a
Moez <86966266+Moez-0@users.noreply.github.com>
2024-11-09 04:35:44 +0100

    Update Hero.tsx

--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { HeroHighlight  , Highlight} from './HeroHighlightDemo'
+import { HeroHighlight, Highlight } from './HeroHighlightDemo'
 
 const Hero = () => {
   return (
     <div className='md:p-24 p-14'>
-        {/* This is a porfolio hero  */}
-        <HeroHighlight >
-          <div className='text-center'>
-            <h1 className='text-4xl font-bold'>Ameny Dhouib</h1>
-            <Highlight className='text-2xl mt-5 '>Software Engineering Student @ SMU</Highlight>
-
-          </div>
-        </HeroHighlight>
-
-        
+      {/* General apology message */}
+      <HeroHighlight>
+        <div className='text-center'>
+          <h1 className='text-4xl font-bold'>I'm Sorry</h1>
+          <Highlight className='text-2xl mt-5'>
+            I just want to make things better between us.
+          </Highlight>
+          <p className='mt-5 text-lg'>
+            You're incredibly important to me, and I never want us to be upset with each other.
+          </p>
+          <p className='mt-5 text-lg italic'>
+            Just know I'm here, and I care about you.
+          </p>
+        </div>
+      </HeroHighlight>
     </div>
   )
 }
