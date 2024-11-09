94c9d483b286aca6c4db2b68cd0fc2ee0f5c12ed
Moez <86966266+Moez-0@users.noreply.github.com>
2024-11-09 04:37:37 +0100

    Update Hero.tsx

--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -7,15 +7,15 @@ const Hero = () => {
       {/* General apology message */}
       <HeroHighlight>
         <div className='text-center'>
-          <h1 className='text-4xl font-bold'>I'm Sorry</h1>
+          <h1 className='text-4xl font-bold'>I&apos;m Sorry</h1>
           <Highlight className='text-2xl mt-5'>
             I just want to make things better between us.
           </Highlight>
           <p className='mt-5 text-lg'>
-            You're incredibly important to me, and I never want us to be upset with each other.
+            You&apos;re incredibly important to me, and I never want us to be upset with each other.
           </p>
           <p className='mt-5 text-lg italic'>
-            Just know I'm here, and I care about you.
+            Just know I&apos;m here, and I care about you. I never want to lose you
           </p>
         </div>
       </HeroHighlight>
